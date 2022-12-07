c4da766d060764c5129c50b6271737cb3c362890
Chloe <chloevision97@gmail.com>
2022-12-07 17:43:21 +0100

    Completed 1to 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...num) {
+  for (const arg of num) {
+    let index = arr.indexOf(arg);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let int1 = 0;
+  let int2 = 0;
+  if (a > b) {
+    int1 = b;
+    int2 = a;
+  } else {
+    int1 = a;
+    int2 = b;
+  }
+  sum = 0;
+  if (typeof int1 === `number` && typeof int2 === `number`) {
+    if (int1 >= 1 && int2 >= 1) {
+      do {
+        sum += int2--;
+      } while (int2 >= 0);
+    } else return "ERROR";
+  } else return "ERROR";
+  // if((typeof int1) === `number` && (typeof int2)  === `number`)
+  return sum;
 };
 
 // Do not edit below this line
