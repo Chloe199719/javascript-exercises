ca54c6c970e8c8c374731f636e34460234914204
Chloe <chloevision97@gmail.com>
2022-12-07 19:41:38 +0100

    Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,50 @@
-const add = function() {
-	
+const add = function (...num) {
+  let sum = 0;
+  for (const arg of num) {
+    sum += arg;
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, ...num) {
+  let sum = a;
+  for (const arg of num) {
+    sum -= arg;
+  }
+
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function (num) {
+  let sum = 0;
+  for (const arg of num) {
+    sum += arg;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (num) {
+  let sum = 1;
+  for (const arg of num) {
+    sum = sum * arg;
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let sum = 1;
+  if (!(a <= 1)) {
+    do {
+      sum = sum * a--;
+    } while (a >= 1);
+  } else return 1;
+  return sum;
 };
-
 // Do not edit below this line
 module.exports = {
   add,
@@ -29,5 +52,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
